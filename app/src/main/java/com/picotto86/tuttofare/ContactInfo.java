c81f9f99f3c22504413155a11a5562d5a506eb11
picotto86 <pavia.mauro@gmail.com>
2014-12-16 23:37:31 +0100

    Initial commit

---
+++ app/src/main/java/com/picotto86/tuttofare/ContactInfo.java
@@ -0,0 +1,15 @@
+package com.picotto86.tuttofare;
+
+/**
+ * Created by picotto86 on 16/12/14.
+ */
+public class ContactInfo {
+    protected String name;
+    protected String surname;
+    protected String email;
+
+
+    protected static final String NAME_PREFIX = "Name_";
+    protected static final String SURNAME_PREFIX = "Surname_";
+    protected static final String EMAIL_PREFIX = "email_";
+}
